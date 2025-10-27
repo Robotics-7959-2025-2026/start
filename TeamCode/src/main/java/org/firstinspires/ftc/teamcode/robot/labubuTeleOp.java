61ca988ffed51df3df429aa41a783d57a934b3b5
Yrgh <adyngoldman@icloud.com>
2025-10-27 15:21:05 -0400

    Small fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/labubuTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/labubuTeleOp.java
@@ -24,11 +24,13 @@ public class labubuTeleOp extends LinearOpMode {
     private double ctrlPow = 1.0;
 
     public void runOpMode() {
-        lfMotor = hardwareMap.get(DcMotor.class, "left");
-        rfMotor = hardwareMap.get(DcMotor.class, "right");
+        // NOTE: Update driver station after 1st comp
+        lfMotor = hardwareMap.get(DcMotor.class, "front_left_drive");
+        rfMotor = hardwareMap.get(DcMotor.class, "front_right_drive");
         rbMotor = hardwareMap.get(DcMotor.class, "back_left_drive");
         lbMotor = hardwareMap.get(DcMotor.class, "back_right_drive");
         shooterMotor = hardwareMap.get(DcMotor.class, "shooterMotor");
+        shooterServo = hardwareMap.get(Servo.class, "shooterServo");
         // TODO!: Get shooterMotor & servo from hardwareMap
 
         lfMotor.setDirection(DcMotorSimple.Direction.FORWARD);
@@ -36,7 +38,6 @@ public class labubuTeleOp extends LinearOpMode {
         lbMotor.setDirection(DcMotorSimple.Direction.FORWARD);
         rbMotor.setDirection(DcMotorSimple.Direction.REVERSE);
         shooterMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
         shooterServo.setDirection(Servo.Direction.REVERSE);
 
         telemetry.addData("Status", "Initialized");
@@ -65,21 +66,6 @@ public class labubuTeleOp extends LinearOpMode {
 
             //lfMotor.setPower(l);
             //rfMotor.setPower(r);
-
-            // Hold left bumper to spin, then press the right bumper to shoot
-            if (gamepad1.left_bumper) {
-                shooterMotor.setPower(1.0);
-
-                if (gamepad1.right_bumper) {
-                    shooterServo.setPosition(15);
-                } else {
-                    shooterServo.setPosition(0);
-                }
-            } else{
-                shooterMotor.setPower(0.0);
-            }
-            
-
         }
     }
 
